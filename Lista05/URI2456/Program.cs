af5daca732a5b7e172a3cc1ca0d7e4f7866529c4
unknown <gabriel0fontineli@gmail.com>
2021-01-17 11:51:27 -0300

    adicionando questoes 2456 e 2473 do URI

--- Lista05/URI2456/Program.cs
+++ Lista05/URI2456/Program.cs
@@ -7,14 +7,22 @@ namespace URI2456
         static void Main(string[] args)
         {
             string [] gplays = Console.ReadLine().Split();
-            int n1,n2,n3,n4,n5;
-            n1 = int.Parse(gplays[0]);n2 = int.Parse(gplays[1]);n3 = int.Parse(gplays[2]); n4 = int.Parse(gplays[3]);n5 = int.Parse(gplays[4]);
-            int[] gg = {n1,n2,n3,n4,n5};
-            int [] certo = gg;
-            Array.Sort(gg);
-
-            if (gg[0] == certo[0] && gg[1] == certo[1] && gg[2] == certo[2] && certo[3] == gg[3])
-            Console.WriteLine("c"); 
+            int[] gg = new int[5];
+            for (int i = 0; i < 5; i++){
+                gg[i]= int.Parse(gplays[i]);
+            }
+            int a = 0; int b = 0; bool C = true; bool D = true;
+            for (int i = 0; i <4; i++){
+                a = gg[i];
+                b = gg[i+1];
+                    if (C && b < a) C = false;
+                    if(D && b > a) D = false;
+                    if (!(D) && !(C)) break;
+            }
+            if(C) Console.WriteLine("C");
+            else if(D)
+            Console.WriteLine("D");
+            else Console.WriteLine("N");
 
                     
         }
---
+++ Lista05/URI2473/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace URI2473
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] aposta = Console.ReadLine().Split();
+            string [] resultado = Console.ReadLine().Split();
+            int aux = 0;
+
+            for(int i = 0; i<6; i++){
+                if(aposta[0] == resultado[i])aux++;
+                else if(aposta[1] == resultado[i])aux++;
+                else if(aposta[2] == resultado[i])aux++;
+                else if(aposta[3]== resultado[i])aux++;
+                else if(aposta[4]== resultado[i])aux++;
+                else if (aposta[5]== resultado[i])aux ++; 
+                
+            };
+            string[] certo = {"azar","azar","azar", "terno","quadra","quina","sena"};
+            Console.WriteLine(certo[aux]);
+        }
+    }
+}
