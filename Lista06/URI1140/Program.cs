db46803aef2e21724afb54a14e450942dbfae766
fonti <gabriel0fontineli@gmail.com>
2021-01-26 14:19:42 -0300

    1140

---
+++ Lista06/URI1140/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace URI1140
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        { 
+          string s;
+          do{
+          s = Console.ReadLine().ToLower();
+          
+          char ini = s[0];
+           int pomba = 0;
+          
+            for (int i = 0;i < s.Length; i++)
+              {
+                if (s=="*") break;
+                else if (s[i] == ' '){
+                char c = s[i+1];
+                  if (c ==ini) {pomba++;}
+                  else {pomba--; break;}
+                }
+              }
+
+           if(pomba < 0) Console.WriteLine("N");
+           else if (pomba > 0)Console.WriteLine("Y");
+          }
+          while(s != "*");
+          
+          }
+    }
+}
