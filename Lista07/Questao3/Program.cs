b4fcf3049a1c0c104b35181e49a4b0052205f918
fonti <gabriel0fontineli@gmail.com>
2021-03-12 09:49:25 -0300

    questao3 resolvida

---
+++ Lista07/Questao3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args){
+            double x = double.Parse(Console.ReadLine());
+            int y,z;
+            Intervalo(x, out y, out z);
+            Console.WriteLine($"[{y}, {z}]");
+
+        }
+        public static void Intervalo(double x, out int inicio, out int fim){
+            inicio = (int) x;
+            fim = inicio+1;
+        }   
+    }
+}
