3f7e2c60ccf36b051eeb6218b8a44e657b1719c1
fonti <gabriel0fontineli@gmail.com>
2021-03-12 17:19:07 -0300

    questao3 resolvida

---
+++ Lista08/I/Questao3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a operação no formato op1+op2:");
+            string[] op = Console.ReadLine().Split('+');
+            int soma = int.Parse(op[0])+ int.Parse(op[1]);
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
