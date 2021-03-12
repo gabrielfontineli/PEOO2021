9ec14fe35c1af83f6716cfbd7f534155aea4a2e7
fonti <gabriel0fontineli@gmail.com>
2021-03-12 17:11:00 -0300

    questao2 resolvida

---
+++ Lista08/I/Questao2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor gasto do combustível em reais:");
+            double gasto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do litro do combustível em reais:");
+            double porlitro = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a distância percorrida em quilômetros:");
+            double distancia = double.Parse(Console.ReadLine());
+            Console.WriteLine($"O consumo médio é de {(distancia/(gasto/porlitro)):.00} Km por litro");
+            Console.WriteLine($"O gasto por Km percorrido é {gasto/distancia:0.00}");
+
+        }
+    }
+}
