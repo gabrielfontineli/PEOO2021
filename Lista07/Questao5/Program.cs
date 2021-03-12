93dad4c75cbadd68cc3592fe10320f07718293ac
fonti <gabriel0fontineli@gmail.com>
2021-03-12 14:51:14 -0300

    adicionando questao incompleta, codigo imprimindo sem espacos

---
+++ Lista07/Questao5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Questao5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(FormatarTexto(s));
+        }
+        public static string FormatarTexto(string texto)
+        {
+            string s = texto;
+            s = s.Trim();
+            int p = s.IndexOf(" ");
+            while(p!= -1)
+            {
+                s = s.Remove(p, 1);
+                p = s.IndexOf(" ");
+            }
+            return s;
+        }
+    }
+}
