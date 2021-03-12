c050d25ea9e3422e9836614925b1f60401627f06
fonti <gabriel0fontineli@gmail.com>
2021-03-12 07:52:08 -0300

    atualizando rep remoto

---
+++ Lista-Revisao01/Questao1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            esfera x = new esfera();
+            x.SetRaio(5);
+            double volume = x.CalcVolume();
+            double area = x.CalcArea();
+            Console.WriteLine(volume);
+            Console.WriteLine(area);
+        }
+    }
+    class esfera 
+    {
+        private double raio;
+        
+        public void SetRaio(double r) {
+        if(r>0)raio = r;}
+
+        public double GetRaio(){
+        return raio;}
+
+        public double CalcArea(){
+        return 4*3.14*Math.Pow(raio,2);}
+
+        public double CalcVolume(){
+        return (3.14*4*Math.Pow(raio,3))/3;}
+    }
+}
