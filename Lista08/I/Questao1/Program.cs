c2bc25057ad63b78ed706dd0f942d39fd80af813
fonti <gabriel0fontineli@gmail.com>
2021-03-12 16:58:38 -0300

    questao1 resolvida

---
+++ Lista08/I/Questao1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do estado:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o número de habitantes:");
+            int habitantes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a área do estado em Km²:");
+            double area = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"A densidade demógrafica do {nome} é de {habitantes/area:.0} hab/km²");
+        }
+    }
+}
