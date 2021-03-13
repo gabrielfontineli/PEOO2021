5a0da9212bb3f7781590a541bf19618a19ba3246
fonti <gabriel0fontineli@gmail.com>
2021-03-13 16:01:56 -0300

    questao2 incompleta

---
+++ POO/Lista01/Questao2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Disciplina x;
+            x = new Disciplina();
+            x.nota1 = 50;
+            x.nota2 = 55;
+            x.nota3 = 56;
+            x.nota4 = 58;
+            x.notafinal= 80;
+            Console.WriteLine(x.MediaParcial());
+        }
+    }
+
+    class Disciplina
+    {
+        public int nota1,nota2,nota3,nota4,notafinal;
+        public int media = ((nota1*2)+ (nota2*2)+ (nota3 * 3)+ (nota4*3))/10;
+
+        public string MediaParcial()
+        {
+                        if (media > 60) return $"Parabéns, você foi aprovado com a média de {media}";
+            else if(media < 60) return $"Que pena, você ficou na prova final com a média {media}, precisando de {120 - media}";
+        }
+        /*public int MediaFinal()
+        {
+        }*/
+    }
+    
+}
+
