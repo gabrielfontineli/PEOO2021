0f38f755c667a032525b74b21a54ba6a1ad0c047
unknown <gabriel0fontineli@gmail.com>
2021-03-13 18:46:57 -0300

    questao3 resolvida

---
+++ POO/Lista01/Questao3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Viagem x = new Viagem();
+            x.SetTempo(2,30);
+            x.SetDistancia(200);
+            Console.WriteLine(x.VelocidadeMedia() + "km/h");
+        }
+    }
+    class Viagem
+    {
+        private double distancia;
+        private double tempo;
+        public void SetDistancia(double d)
+        {
+            if (d>0) distancia = d; 
+        }
+        public void SetTempo(int h, double m)
+        {
+            if(m > 0 && h > 0 ) tempo = h + (m/60);
+        }
+        public double VelocidadeMedia()
+        {
+            return distancia/tempo;
+        }
+        
+    }
+}
