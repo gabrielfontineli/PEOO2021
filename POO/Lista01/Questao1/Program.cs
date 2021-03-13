a5168a6b9b41de2fb30e0396012a6a002b48647c
fonti <gabriel0fontineli@gmail.com>
2021-03-13 11:13:01 -0300

    questao1-poo1

---
+++ POO/Lista01/Questao1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            circulo x;
+            x = new circulo();
+            x.raio = 3;
+            Console.WriteLine(x.CalcArea());
+            Console.WriteLine(x.CalcCirc());
+        }
+    }
+    class circulo
+    {
+        public double raio;
+        public double CalcArea()
+        {
+            return Math.PI * Math.Pow(raio,2);
+        }
+        public double CalcCirc()
+        {
+            return Math.PI * 2 * raio;
+        }
+    }
+}
