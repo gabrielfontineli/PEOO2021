27df280a91312213dccb036011220466065654e4
unknown <gabriel0fontineli@gmail.com>
2021-03-19 19:48:41 -0300

    adicionando questao01-lista02

---
+++ POO/Lista02/Questao1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo x = new Circulo();
+            x.SetRaio(5);
+            Console.WriteLine($"a area eh: {x.CalcArea():.00}");
+            Console.WriteLine($"a circunferencia eh: {x.CalcCircunferencia():.00}");
+        }
+    }
+    class Circulo
+    {
+        private double raio;
+        public void SetRaio(double entradaraio)
+        {
+            this.raio = entradaraio;
+        }
+        public double GetRaio(){
+            return raio;
+        }
+        public double CalcArea(){
+            return Math.PI * Math.Pow(raio,2);
+        }
+        public double CalcCircunferencia(){
+            return 2 * Math.PI*raio;
+        }
+    }
+}
