f9b4c2a0e4d960248be44875a25d8e3a0aadd2a6
unknown <gabriel0fontineli@gmail.com>
2021-03-20 16:18:06 -0300

    questao incompleta vai tomar no cu gg

---
+++ POO/Lista02/Questao4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco x = new Banco();
+            x.SetNome("Gabriel");
+            x.SetConta(895945);
+            x.SetSaldo(3000);
+            Console.WriteLine(x.GetSaldo());
+            x.Deposito(500);
+            Console.WriteLine(x.GetSaldo());
+            x.Saque(-500);
+            Console.WriteLine(x.GetSaldo());
+        }
+    }
+    class Banco
+    {
+        private string nome; private double conta,saldo;
+        public void SetNome(string n)
+        {
+            nome = n;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public void SetConta(double n)
+        {
+            conta = n;
+        }
+        public double GetConta()
+        {
+            return conta;
+        }
+        public void SetSaldo(double s)
+        {
+            saldo = s;
+        }
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+        public double Saque(double n)
+        {
+            if (n <= saldo && n >0) saldo -= n;
+            return n;
+        }
+        public double Deposito(double n)
+        {
+            if (n > 0) saldo+=n;
+            return n;
+        }
+    }
+}
