a8229f62a1c57c9886c6c369701c2ca2c3f329a1
unknown <gabriel0fontineli@gmail.com>
2021-03-22 09:17:16 -0300

    questao do retangulo

---
+++ POO/Lista03/Questao1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo(10,20);
+            x.SetAltura(3);
+            x.SetBase(4);
+            Console.WriteLine(x.CalcArea());
+        }
+    }
+    class Retangulo
+    {
+        private double b,h;
+        public Retangulo(double b, double h)
+        {
+            if(b > 0) this.b = b;
+            if(h > 0) this.h = h;
+        }
+        public void SetBase(double b)
+        {
+            if(b > 0) this.b = b;
+        }
+        public double GetBase()
+        {
+            return b;
+        }
+        public void SetAltura(double h)
+        {
+            if(h > 0) this.h = h;
+        }
+        public double GetAltura()
+        {
+            return h;
+        }
+        public double CalcArea()
+        {
+            return b*h;
+        }
+        public double CalcDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(b,2)+ Math.Pow(h,2));
+        }
+        public override string ToString()
+        {
+            return $"Base = {b}, Altura = {h}";
+        }
+    }
+}
