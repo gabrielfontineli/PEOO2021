f7f71eeeedeec14751cd7883249944b9358e78d2
unknown <gabriel0fontineli@gmail.com>
2021-03-23 10:09:42 -0300

    questao resolvida

---
+++ POO/Lista03/Questao2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Frete x = new Frete(500,4);
+            x.SetDistancia(400);
+            x.SetPeso(10);
+            Console.WriteLine(x.CalcFrete());
+        }
+    }
+    class Frete
+    {
+        private double distancia,peso;
+        public Frete(double d, double p)
+        {
+            if (d > 0) distancia = d;
+            if (p > 0) peso = p;
+        }
+        public void SetDistancia (double d)
+        {
+            if(d > 0) distancia = d;
+        }
+        public double GetDistancia()
+        {
+            return distancia;
+        }
+        public void SetPeso(double p)
+        {
+            if(p > 0) peso = p;
+        }
+        public double GetPeso()
+        {
+            return peso;
+        }
+        public double CalcFrete()
+        {
+            //1 centavo para cada quilo por km
+            return (distancia * peso)/100 ;
+        }
+    }
+}
